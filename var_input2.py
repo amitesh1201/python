8722f253eccc7cdd0fed21885799694bedf03a6a
swapnil <swapnilzarbade@gmail.com>
2015-10-22 22:35:32 +0530

    var_input2.py script added.

---
+++ var_input2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+from sys import argv
+
+script, user_name = argv
+prompt = '>'
+
+print "Hi %s.This is  %s script" % (user_name, script)
+#likes = raw_input(prompt)
+
+print "Enter your age %s" % user_name
+likes = raw_input(prompt)
+
+print "Enter your education %s" % user_name
+likes = raw_input(prompt)
+
+
