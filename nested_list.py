58e9f8fce2ca385ddd597d281df39d4d3432243b
Swapnil <swapnilz30@users.noreply.github.com>
2017-09-14 16:00:48 +0530

    nested_list.py added

---
+++ nested_list.py
@@ -0,0 +1,15 @@
+#!/usr/local/bin/python
+
+my_list = ["mouse",[1,2,3],['a']]
+print my_list
+print my_list[1]
+print "OUTPUT:"
+print my_list[1][2]
+list1 = my_list[1]
+print "list1:"
+print list1[1]
+
+print "------------------"
+
+for item in my_list:
+    print item
