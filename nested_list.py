8c8cac00ea97288fdaceb8119fcf8436acedf2e6
Swapnil <swapnilz30@users.noreply.github.com>
2017-09-14 16:06:18 +0530

    Update nested_list.py

--- nested_list.py
+++ nested_list.py
@@ -3,6 +3,8 @@
 my_list = ["mouse",[1,2,3],['a']]
 print my_list
 print my_list[1]
+print "0 to 1:"
+print (my_list[0:1])
 print "OUTPUT:"
 print my_list[1][2]
 list1 = my_list[1]
@@ -13,3 +15,7 @@ print "------------------"
 
 for item in my_list:
     print item
+
+my_list = ['p','r','o','g','r','a','m','i','z']
+# elements 3rd to 5th
+print(my_list[2:5])
