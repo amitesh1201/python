358a4cd33090bb04655fbc2ab1ae7ce96f9660c2
swapnil <swapnilzarbade@gmail.com>
2018-11-17 18:50:12 +0530

    Added create directory code.

--- create-dir.py
+++ create-dir.py
@@ -2,5 +2,11 @@
 
 # This script creates the directory.
 
-dir_name =  input("Enter dir path")
-print (dir_name)
+dir_name =  input("Enter dir path: ")
+
+try:
+    # Create the target directory.
+    os.mkdir(dir_name)
+    print ("Directory ", dir_name , " created ")
+except FileExistsError:
+    print("Directory ", dir_name , " already exist")
