a1752aca6b14c8a424eeb714d2adba31ba8cc1d5
swapnil <swapnilzarbade@gmail.com>
2018-11-17 18:27:55 +0530

    Deleted all file.

--- add.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-person = raw_input ('enter name: ')
-sum1 = int(person) + 1
-print (sum1)
--- dictionary.py
+++
@@ -1,7 +0,0 @@
-#!/usr/local/bin/python
-
-dict = {'Name': 'Swapnil', 'Age': 33, 'Occupation': 'Service'}
-
-print dict['Name']
-
-print dict['Age']
