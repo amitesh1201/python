32055258eb6655838cfa37f3c69c37318ad40c94
swapnil <swapnilzarbade@gmail.com>
2018-11-18 15:21:52 +0530

    Added the file create-file.py

---
+++ create-file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.6
+
+# This script create the file.
+
+import os
+file_name = input("Enter file name")
+
+f = open(file_name,'w')
+f.close()
+print("The file ", file_name, "is created.")
