7d7789c0b0cfbe6c1462da38f160d510d816baa2
swapnil <swapnilzarbade@gmail.com>
2018-11-22 00:03:15 +0530

    Added the menu-operations.py file

---
+++ menu-operations.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3.6
+
+# This script perform the maths operations.
+
+import os
+from sys import exit
+
+global menu_condition
+
+def addition(num1,num2):
+    result = num1 + num2
+    return result
+
+def substration(num1,num2):
+    result = num1 - num2
+    return result
+
+def multiplication(num1,num2):
+    result = num1 * num2
+    return result
+
+def devision(num1,num2):
+    if num2 == 0:
+       print ("Denominator should not be zero")
+       exit()
+
+    result = num1 / num2
+    return result
+
+def menu():
+    while menu_condition == 0:
+        print("Select the choice for operations")
+        print("Select 1 for addition")
+        print("select 2 for substration")
+        print("select 3 for multiplication")
+        print("select 4 for devision")
+        print("select 5 for exit")
+
+        menu_condition = input("Select the choice: ")
+        num1 = input("Enter num1: ")
+        num2 = input("Enter num2: ")
+
+        if menu_condition == 1:
+            result = addition(num1,num2)
+            print ("The addition of ", num1 ,"+",num2,"is",result)
+        elif menu_condition == 2:
+            substration(num1,num2)
+            print ("The substration of ", num1, "-", num2, "is", result)
+        elif menu_condition == 3:
+            multiplication(num1,num2)
+            print ("The multiplication of ", num1, "*", num2, "is", result)
+        elif menu_condition == 4:
+            devision(num1,num2)
+            print ("The devision of ", num1, "/", num2, "is", result)
+        elif menu_condition == 5:
+            exit()
+        else:
+            print("Select choice from the menu")
+
+
+### Main ###
+menu_condition=0
+menu()
