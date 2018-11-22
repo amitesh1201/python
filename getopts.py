edaf0a15eff2348d09b8496d9071c6f80559a56b
swapnil <swapnilzarbade@gmail.com>
2018-11-23 00:36:09 +0530

    Added info_msg() function

--- getopts.py
+++ getopts.py
@@ -4,17 +4,20 @@
 
 import sys, getopt
 
+def info_msg():
+    print ("getopts.py -i <inputfile -o outputfile>")
+
 def main(argv):
     inputfile = ''
     outputfile = ''
     try:
         opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
     except getopt.GetoptError:
-        print ("getopts.py -i <inputfile -o outputfile>")
+        info_msg()
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ("getopts.py -i <inputfile -o outputfile>")
+            info_msg()
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
