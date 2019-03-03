5e9aff7fc4be4fadda353a5cf1fb5e48a359b419
root <root@localhost.localdomain>
2019-03-03 21:38:43 +0530

    Display the data from postgresql python_insert_csv_postgres.py

--- python_insert_csv_postgres.py
+++ python_insert_csv_postgres.py
@@ -2,24 +2,40 @@
 
 import subprocess
 import os
-import psycopg2
+import csv
+import fnmatch
+import psycopg2 as pg
 import pandas as pd
+from sqlalchemy import create_engine
 
 #conn = psycopg2.connect(database = "all_network", user = "postgres", password = "redhat", host = "127.0.0.1", port = "5432")
+conn = pg.connect("host='localhost' dbname='testdb' user='postgres' password='redhat'")
+table_data = pd.read_sql('SELECT * FROM all_macs',conn)
+print (table_data)
+#cur = conn.cursor()
+#cur.execute("SELECT * FROM all_macs")
+#conn.close()
 #status, conn = subprocess.getstatusoutput('psycopg2.connect(user = "'"postgres"'", password = "'"redhat"'", host = "'"127.0.0.1"'", port = "'"5432"'", database = "'"testdb"'")')
 #status, result = subprocess.getstatusoutput('ls -l /root/python/all-network-04.csv')
 
 #print (status)
 #print("connected")
 
-file_name = input("Eneter filename: ")
-table_name = input("Enter table name to create: ")
-
-#df = pd.read_csv('all-network-04.csv')
-df = pd.read_csv(file_name)
-df.columns = [c.lower() for c in df.columns] #postgres doesn't like capitals or spaces
-
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://postgres:redhat@localhost:5432/testdb')
-
-df.to_sql(table_name, engine)
+#file_name = input("Eneter filename: ")
+#db_name = input("Enter database name: ")
+#table_name = input("Enter table name to create: ")
+#
+#if (os.path.isfile(file_name.strip()):
+#   if fnmatch.fnmatch(file_name,'*.csv')
+#      # Check the database exist or not.
+#      # Check table exist or not.
+#      #df = pd.read_csv('all-network-04.csv')
+#      df = pd.read_csv(file_name)
+#      df.columns = [c.lower() for c in df.columns] #postgres doesn't like capitals or spaces
+#      engine = create_engine('postgresql://postgres:redhat@localhost:5432/testdb')
+#      df.to_sql(table_name, engine)
+#   else:
+#      print("The ",file_name," is not is csv format")
+#else:
+#    print("The ", file_name," not exist")
+#
