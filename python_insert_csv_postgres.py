567cdc45d5152380849b7f041d1c923cf9b15436
root <root@localhost.localdomain>
2019-03-03 23:22:35 +0530

    Added database variable

--- python_insert_csv_postgres.py
+++ python_insert_csv_postgres.py
@@ -8,21 +8,8 @@ import psycopg2 as pg
 import pandas as pd
 from sqlalchemy import create_engine
 
-#table_name = 'all_macs'
-#conn = psycopg2.connect(database = "all_network", user = "postgres", password = "redhat", host = "127.0.0.1", port = "5432")
-#conn = pg.connect("host='localhost' dbname='testdb' user='postgres' password='redhat'")
-#table_data = pd.read_sql('SELECT * FROM all_macs',conn)
-#table_data = pd.read_sql('SELECT datname FROM pg_catalog.pg_database',conn)
-#print (table_data)
-#cur = conn.cursor()
-#cur.execute("select exists(select * from information_schema.tables where table_name=%s)", (table_name,))
-#print (cur.fetchone()[0])
-#conn.close()
-#status, conn = subprocess.getstatusoutput('psycopg2.connect(user = "'"postgres"'", password = "'"redhat"'", host = "'"127.0.0.1"'", port = "'"5432"'", database = "'"testdb"'")')
 #status, result = subprocess.getstatusoutput('ls -l /root/python/all-network-04.csv')
 
-#print (status)
-#print("connected")
 
 file_name = input("Eneter filename: ")
 db_name = input("Enter database name: ")
@@ -30,11 +17,9 @@ table_name = input("Enter table name to create: ")
 
 if (os.path.isfile(file_name.strip())):
         if fnmatch.fnmatch(file_name,'*.csv'):
-             # Check the database exist or not.
-             # Check table exist or not.
              #df = pd.read_csv('all-network-04.csv')
 
-             conn = pg.connect("host='localhost' dbname='testdb' user='postgres' password='redhat'")
+             conn = pg.connect(host='localhost', dbname=db_name, user='postgres', password='redhat')
              table_data = pd.read_sql('SELECT datname FROM pg_catalog.pg_database',conn)
              cur = conn.cursor()
              cur.execute("select exists(select * from information_schema.tables where table_name=%s)", (table_name,))
