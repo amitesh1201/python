47dac8550b47f75105bed4ab307dbe7a43f978e5
root <root@localhost.localdomain>
2019-03-09 23:27:15 +0530

    Displayed the databases in single row

--- python_menu_postgres.py
+++ python_menu_postgres.py
@@ -14,21 +14,32 @@ db_name = 'testdb'
 user_name = 'postgres'
 user_password = 'redhat'
 
-def connect_db(host_name,db_name,user_name,user_password):
 #def connect_db():
-
 #    conn = pg.connect(host='localhost', dbname='testdb', user='postgres', password='redhat')
+#    show_databases(conn)
+
+def connect_db(host_name,db_name,user_name,user_password):
+    # Connect the database
     conn = pg.connect(host=host_name, dbname=db_name, user=user_name, password=user_password)
-    #show_databases(conn)
-    print ("connected")
+    print ("Database connected")
+    print("====================")
     return conn
 
 def show_databases():
      
-    print("1. Show databases")
+    conn = connect_db(host_name,db_name,user_name,user_password)
+    cur = conn.cursor()
+    cur.execute('SELECT datname FROM pg_catalog.pg_database')
+    table_data = cur.fetchall()
+    print("Database: ")
+    for td in table_data:
+        print(td[0])
+    conn.close()
     #table_data = pd.read_sql('SELECT datname FROM pg_catalog.pg_database',conn)
-    table_data = pd.read_sql('SELECT datname FROM pg_catalog.pg_database',connect_db(host_name,db_name,user_name,user_password))
-    print(table_data)
+    #table_data = pd.reconnect_db(host_name,db_name,user_name,user_password)ad_sql('SELECT datname FROM pg_catalog.pg_database',connect_db(host_name,db_name,user_name,user_password))
+    #td = table_data.set_index("datname", drop = False)
+    #print(table_data)
+    #print(table_data.Name.to_string(index=False))
 
 
 def menu():
@@ -55,8 +66,8 @@ def menu():
            exit() 
            
 
-#menu()         
+menu()         
 #connect_db()
 #connect_db(host_name,db_name,user_name,user_password)
-show_databases()
+#show_databases()
 
