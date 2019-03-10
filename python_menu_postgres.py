6b9d9cb08259951352c63e733700ebe12ea23867
root <root@localhost.localdomain>
2019-03-10 13:36:22 +0530

    Added show_table_contents function

--- python_menu_postgres.py
+++ python_menu_postgres.py
@@ -36,6 +36,14 @@ def get_list_tables():
     table_sql_query = "SELECT table_name FROM information_schema.tables where table_schema='public'"  
     return table_sql_query
 
+def get_table_name():
+    table_name = input ("Enter table name to display the contents: ")
+    return table_name
+
+def get_table_data(table_name):
+    table_data_sql_query = "SELECT * FROM " + table_name
+    return table_data_sql_query
+
 def connect_db(host_name,db_name,user_name,user_password):
     # Connect the database
     conn = pg.connect(host=host_name, dbname=db_name, user=user_name, password=user_password)
@@ -95,6 +103,43 @@ def show_tables():
     for td in table_data:
         print(td[0])
 
+def show_table_contents():
+    # Display the table data of selected table.
+    
+    
+    flag = 0
+
+    # If the entered database is not exist then exit the script.
+    db_name = get_dbname()
+    host_name = get_hostname()
+    user_name = get_username()
+    user_password = get_password()
+    user_table = get_table_name()
+
+    #db_name = "testdb"
+    #host_name = "localhost"
+    #user_name = "postgres"
+    #user_password = "redhat"
+    #user_table = "all_macs"
+
+    # If the entered table name is not exist then exit the script.
+    conn = connect_db(host_name,db_name,user_name,user_password)
+    table_data = get_sql_result(get_list_tables(),conn) 
+    for td in table_data:
+        if td[0] == user_table:
+           flag = 1
+           break
+        
+    if flag == 0:
+       print("The table", user_table, "not exist.Enter valid table name")
+       exit()
+
+    conn = connect_db(host_name,db_name,user_name,user_password)
+    table_data = get_table_data(user_table)
+    print(table_data)
+    table_data_result = get_sql_result(table_data,conn)
+    print(table_data_result)
+
     
 def menu():
     # Display the database operations menu
@@ -107,7 +152,7 @@ def menu():
     while menu_option == True:
         print ("1. Show databases")
         print ("2. Show tables")
-        print ("Option 3")
+        print ("3. Show table contents")
         
         opt = int(input("Select the option: "))
         
@@ -117,6 +162,8 @@ def menu():
         if opt == 2:
            show_tables() 
 
+        if opt == 3:
+           show_table_contents()
 
         flag = input("Do you wish to continue: (y/n) ")
 
