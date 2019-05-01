c993ff8a337c01ace31487d5f634d24e568a1b1d
swapnil <swapnilzarbade@gmail.com>
2019-05-01 18:54:46 +0530

    Added git_operations.py file

---
+++ git_operations.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3.6
+
+# This file performs the git operations.
+
+import sys, getopt, os
+
+def get_branch():
+    os.system("git status")
+
+
+
+if __name__ == '__main__':
+    get_branch()
+
