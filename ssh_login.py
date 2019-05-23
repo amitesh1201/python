7791913cd807e4f5d84ec1c82fc00ec57f5b1e00
swapnil <swapnilzarbade@gmail.com>
2019-05-23 20:46:57 +0530

    Added code to any ip login

--- ssh_login.py
+++ ssh_login.py
@@ -9,13 +9,16 @@ def usage():
     print("-s: Enter user name to login")
     print("-f: Login to 192.168.50.x")
     print("-s: Login to 192.168.60.x")
+    print("-a:  Enter any IP to login")
 
 def login(user_name,user_ip):
+    if not user_name or not user_ip:
+        print("The username or user ip should not be empty")
     os.system("ssh " + user_name + "@" + user_ip)
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"u:f:s:h")
+        opts, args = getopt.getopt(argv,"u:f:s:a:h")
         if not opts:
             usage()
             exit(2)
@@ -38,8 +41,16 @@ def main(argv):
                 usage()
                 exit (2)
             user_ip = "192.168.60."+arg
+        elif opt in ("-a"):
+            if not arg:
+                usage()
+                exit(2)
+            user_ip = arg
+        elif opt in ("-h"):
+            usage()
+            exit(2)
+    login(user_name, user_ip)
 
-    login(user_name,user_ip)
 
 if __name__ == '__main__':
     try:
